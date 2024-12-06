242985446da720d8eb17b7cfe5e2c19936febc2f
Mouli1807 <74283228+Mouli1807@users.noreply.github.com>
2024-12-06 10:41:40 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+const HeadingComponent = () => (
+    <h1 className="heading" >Hello Mouli</h1>
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>); 
